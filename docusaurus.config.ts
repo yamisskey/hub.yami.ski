9f3560816310c63ae3c8f16ab0768513cb6042a3
hitalin <hitalin@proton.me>
2024-07-24 08:10:32 +0900

    update: Change OGP image from Docusaurus default

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/ogBanner.jpg',
     navbar: {
       title: 'yamisskey',
       logo: {
